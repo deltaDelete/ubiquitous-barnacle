b73160534624854b8b37834640eecd3a59a86ccc
deltaDelete <samansy@yandex.ru>
2024-03-05 00:59:31 +0700

    feat: full crud

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,35 @@ interface City {
 function App() {
     const [cities, setCities] = useState<City[]>([])
     const [loading, setLoading] = useState(false);
+    const [isEdit, setIsEdit] = useState(false)
+
+    const [selectedIndex, setSelectedIndex] = useState(0)
 
     const {
         register,
         handleSubmit,
         watch,
         formState: {errors},
+        setValue
     } = useForm<City>()
 
     const onSubmit: SubmitHandler<City> = (data) => {
 
+        if (isEdit) {
+            fetch(`/api/city/${data.cityId}`, {
+                method: "PUT",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(result => result.ok ? result.json() : undefined)
+                .then(data => {
+                    cities[selectedIndex] = data;
+                    setCities(cities);
+                });
+            return;
+        }
         fetch("/api/city", {
             method: "POST",
             body: JSON.stringify(data),
@@ -49,29 +68,45 @@ function App() {
             method: "DELETE"
         }).then(result => {
             if (result.ok) {
-                const index = cities.indexOf(item);
-                delete cities[index];
+                delete cities[self.props.index];
                 setCities([...cities]);
             }
         })
     }
-    
+
     const onEdit = (self: CityItem, item: City) => {
-        // TODO
+        setValue("name", item.name);
+        setValue("cityId", item.cityId);
+        setSelectedIndex(self.props.index);
+        setIsEdit(true);
+    }
+
+    const cancel = () => {
+        setValue("cityId", 0);
+        setValue("name", "");
+        setSelectedIndex(0);
+        setIsEdit(false);
     }
 
     return (
         <div className="flex flex-col gap-2">
             <div>
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
-                    <input defaultValue="" {...register("name")}
-                           className="text-black border-black border-2 rounded-md p-2" type="text"/>
-                    <input type="submit" className="p-2 bg-orange-400 rounded-md hover:bg-slate-700 hover:text-white"/>
+                    <input type="hidden" {...register("cityId")} />
+                    <input defaultValue="" {...register("name", {required: true, minLength: 5})}
+                           aria-invalid={errors.name ? "true" : "false"}
+                           className="text-black aria-invalid:border-red-500 border-black border-2 rounded-md p-2" type="text"/>
+                    <input value={isEdit ? "Изменить" : "Добавить"} type="submit"
+                           aria-disabled={typeof errors != typeof {}}
+                           className="p-2 bg-orange-400 aria-disabled:bg-gray-600 rounded-md hover:bg-slate-700 hover:text-white"/>
+                    {errors.name ? <p>{errors.name.type}</p> : <p></p>}
+                    <button type="button" className={isEdit ? "static" : "hidden"} onClick={cancel}>Отмена</button>
                 </form>
             </div>
             <div className="card">
-                <div className="flex flex-col gap-1">
-                    {cities.map((c) => c && <CityItem city={c} onRemoveCallback={onRemove} onEditCallback={onEdit}/>)}
+                <div className="flex flex-col gap-1 max-h-[16rem] overflow-x-clip overflow-y-scroll">
+                    {cities.map((c, index) => c &&
+                        <CityItem city={c} index={index} onRemoveCallback={onRemove} onEditCallback={onEdit}/>)}
                 </div>
             </div>
         </div>
@@ -80,6 +115,7 @@ function App() {
 
 type CityItemProps = {
     city: City,
+    index: number,
     onRemoveCallback: ((self: CityItem, item: City) => void) | undefined,
     onEditCallback: ((self: CityItem, item: City) => void) | undefined
 }
@@ -88,12 +124,17 @@ class CityItem extends Component<CityItemProps, ComponentState> {
     render() {
         return (
             <div key={this.props.city.cityId}
-                 className='group/item h-[4rem] w-[16rem] flex flex-col content-center items-center justify-center p-2 rounded-md bg-slate-700 text-white text-center'>
-                <span className="group-hover/item:hidden">{this.props.city.name}</span>
-                <div className="group/actions hidden group-hover/item:flex items-center flex-row gap-2">
+                 className='group/item relative h-[4rem] w-[16rem] flex flex-col content-center items-center justify-center p-2 rounded-md bg-slate-700 text-white text-center'>
+                <span className="group-hover/item:invisible absolute">{this.props.city.name}</span>
+                <div
+                    className="group/actions invisible relative group-hover/item:flex group-hover/item:visible items-center flex-row gap-2">
                     <span>Id: {this.props.city.cityId}</span>
-                    <button onClick={() => this.removeSelf(this.props.city)} className="bg-slate-900 text-white hover:bg-red-900">DELETE</button>
-                    <button onClick={() => this.editSelf(this.props.city)} className="bg-slate-900 text-white hover:bg-sky-900">EDIT</button>
+                    <button onClick={() => this.removeSelf(this.props.city)}
+                            className="bg-slate-900 text-white hover:bg-red-900">DELETE
+                    </button>
+                    <button onClick={() => this.editSelf(this.props.city)}
+                            className="bg-slate-900 text-white hover:bg-sky-900">EDIT
+                    </button>
                 </div>
             </div>
         )
@@ -102,7 +143,7 @@ class CityItem extends Component<CityItemProps, ComponentState> {
     removeSelf(item) {
         this.props.onRemoveCallback?.call(null, this, item);
     }
-    
+
     editSelf(item) {
         this.props.onEditCallback?.call(null, this, item)
     }
